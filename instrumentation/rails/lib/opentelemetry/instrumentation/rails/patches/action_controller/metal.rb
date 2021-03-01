8d4a1af9560120517f709dbe598664a360e7503d
Ryan Rosenblum <rrosenblum@splunk.com>
2021-03-01 09:14:36 -0500

    hacky change to test supporting rails 4.2

--- instrumentation/rails/lib/opentelemetry/instrumentation/rails/patches/action_controller/metal.rb
+++ instrumentation/rails/lib/opentelemetry/instrumentation/rails/patches/action_controller/metal.rb
@@ -11,10 +11,10 @@ module OpenTelemetry
         module ActionController
           # Module to prepend to ActionController::Metal for instrumentation
           module Metal
-            def dispatch(name, request, response)
+            def dispatch(name, request)
               rack_span = OpenTelemetry::Instrumentation::Rack.current_span
               rack_span.name = "#{self.class.name}##{name}" if rack_span.context.valid? && !request.env['action_dispatch.exception']
-              super(name, request, response)
+              super
             end
           end
         end
